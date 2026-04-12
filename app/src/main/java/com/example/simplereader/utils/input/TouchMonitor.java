5423f703314f85541728357e7b0f2a565ef0ee79
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 06:40:31 +0800

    Add app/src/main/java/com/example/simplereader/utils/input/TouchMonitor.java

---
+++ app/src/main/java/com/example/simplereader/utils/input/TouchMonitor.java
@@ -0,0 +1,16 @@
+/*
+ * TouchMonitor - 触摸监控器
+ */
+package com.example.simplereader.utils.input;
+
+public class TouchMonitor {
+    private boolean isTouching = false;
+    
+    public boolean isTouching() {
+        return isTouching;
+    }
+    
+    public void setTouching(boolean touching) {
+        this.isTouching = touching;
+    }
+}
