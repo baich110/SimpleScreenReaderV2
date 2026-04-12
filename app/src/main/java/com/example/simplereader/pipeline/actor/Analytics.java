2bd4fa301363f7c1e29feaae326cca51effb3484
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 06:40:08 +0800

    Add app/src/main/java/com/example/simplereader/pipeline/actor/Analytics.java

---
+++ app/src/main/java/com/example/simplereader/pipeline/actor/Analytics.java
@@ -0,0 +1,11 @@
+/*
+ * Analytics - 分析
+ */
+package com.example.simplereader.pipeline.actor;
+
+import com.example.simplereader.pipeline.Performance;
+
+public class Analytics {
+    public void onSingleTap(Performance.EventId eventId) {}
+    public void onEvent(int eventType) {}
+}
