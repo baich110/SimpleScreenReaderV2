ff685f20fc15839dd7cee192b068e8bd188a5667
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 23:33:57 +0800

    Add TouchExplorationAction enum

--- app/src/main/java/com/example/simplereader/pipeline/interpreters/TouchExplorationListener.java
+++ app/src/main/java/com/example/simplereader/pipeline/interpreters/TouchExplorationListener.java
@@ -5,3 +5,12 @@ import com.example.simplereader.pipeline.Performance;
 public interface TouchExplorationListener {
     boolean onTouchExplorationAction(TouchExplorationAction action, Performance.EventId eventId);
 }
+
+enum TouchExplorationAction {
+    TOUCH_DOWN,
+    TOUCH_UP,
+    HOVER_ENTER,
+    HOVER_EXIT,
+    GESTURE_START,
+    GESTURE_END
+}
