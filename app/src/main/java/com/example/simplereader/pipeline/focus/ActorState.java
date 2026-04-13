c46bc9b5bcc4b8942b01ce670472f81e0bb249ae
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-14 03:58:46 +0800

    Restore ActorState.java to working version

---
+++ app/src/main/java/com/example/simplereader/pipeline/focus/ActorState.java
@@ -0,0 +1,6 @@
+package com.example.simplereader.pipeline.focus;
+
+import com.example.simplereader.pipeline.ActorState;
+
+public class ActorState extends com.example.simplereader.pipeline.ActorState {
+}
