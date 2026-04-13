4f8866fe064654329e68349fb8b17e48706b91b4
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 21:25:37 +0800

    Add FeatureFlagReader.java - Feature flag reader

---
+++ app/src/main/java/com/example/simplereader/utils/FeatureFlagReader.java
@@ -0,0 +1,7 @@
+package com.example.simplereader.utils;
+
+import android.content.Context;
+
+public class FeatureFlagReader {
+    public static boolean splitTapEverywhere(Context context) { return false; }
+}
