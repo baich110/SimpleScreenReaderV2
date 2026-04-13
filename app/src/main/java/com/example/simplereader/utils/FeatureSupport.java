eef0c2a6d628ff87c8525b9f2097633a9ca87249
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 21:25:35 +0800

    Add FeatureSupport.java - Feature support detection

---
+++ app/src/main/java/com/example/simplereader/utils/FeatureSupport.java
@@ -0,0 +1,6 @@
+package com.example.simplereader.utils;
+
+public class FeatureSupport {
+    public static boolean supportSplitTapEverywhere() { return false; }
+    public static boolean isAndroidWear() { return false; }
+}
