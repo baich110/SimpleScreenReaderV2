16ce61be0afc17934c735f78cc7df032b289ee20
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 23:33:44 +0800

    Remove duplicate ActorState class

--- app/src/main/java/com/example/simplereader/pipeline/focus/ActorState.java
+++
@@ -1,6 +0,0 @@
-package com.example.simplereader.pipeline.focus;
-
-import com.example.simplereader.pipeline.ActorState;
-
-public class ActorState extends com.example.simplereader.pipeline.ActorState {
-}
