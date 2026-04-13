e273c91e55073c8978ac8ce751dfed027ff32928
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-14 03:36:32 +0800

    Restore InputFocusListener.java from working version f79eed5

--- app/src/main/java/com/example/simplereader/pipeline/interpreters/InputFocusListener.java
+++ app/src/main/java/com/example/simplereader/pipeline/interpreters/InputFocusListener.java
@@ -2,8 +2,8 @@ package com.example.simplereader.pipeline.interpreters;
 
 import com.example.simplereader.pipeline.Performance;
 import android.view.accessibility.AccessibilityEvent;
-import androidx.annotation.Nullable;
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public interface InputFocusListener {
     void onViewTargeted(@Nullable Performance.EventId eventId, AccessibilityEvent event, AccessibilityNodeInfoCompat targetedNode);
