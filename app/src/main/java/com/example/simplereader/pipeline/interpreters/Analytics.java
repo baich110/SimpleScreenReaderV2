f332b69c2e844882ddc0220fc0c1c0313dd87384
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 21:25:59 +0800

    Add Analytics.java - Analytics tracking

---
+++ app/src/main/java/com/example/simplereader/pipeline/interpreters/Analytics.java
@@ -0,0 +1,7 @@
+package com.example.simplereader.pipeline.interpreters;
+
+import com.example.simplereader.pipeline.Performance;
+
+public class Analytics {
+    public void onSingleTap(Performance.EventId eventId) {}
+}
