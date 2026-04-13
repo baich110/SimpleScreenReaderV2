16626d1647bb58a23ecd3fceb2ecea985340c70c
baich110 <154077481+baich110@users.noreply.github.com>
2026-04-13 21:30:52 +0800

    Add TraversalStrategy.java

--- app/src/main/java/com/example/simplereader/utils/traversal/TraversalStrategy.java
+++ app/src/main/java/com/example/simplereader/utils/traversal/TraversalStrategy.java
@@ -1,13 +1,9 @@
-/*
- * TraversalStrategy - 遍历策略
- */
 package com.example.simplereader.utils.traversal;
 
 public class TraversalStrategy {
     public static final int SEARCH_FOCUS_FORWARD = 1;
     public static final int SEARCH_FOCUS_BACKWARD = 2;
     public static final int SEARCH_FOCUS_UNKNOWN = 0;
-    
     public static final int MOVEMENT_GRANULARITY_CHARACTER = 1;
     public static final int MOVEMENT_GRANULARITY_WORD = 2;
     public static final int MOVEMENT_GRANULARITY_LINE = 4;
